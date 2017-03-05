5678ba119d21b270d8a6780f7088ec66f3b6f72d
Deepak Kannan <kannan.deepak@gmail.com>
2017-03-05 18:14:17 +0530

    Add question about multiple cases

--- questions.erl
+++ questions.erl
@@ -1,5 +1,5 @@
 -module(questions).
--export([bigNumFails/0]).
+-export([bigNumFails/0, howToWriteMultipleCase/0]).
 
 bigNumFails() ->
 	% math:pow(2,1023).
@@ -20,3 +20,39 @@ bigNumFails() ->
 % but import goes through fine. how to catch such issues ?
 foo(x) ->
 	x.
+
+%% does not compile
+%% error is, "questions.erl:36: variable 'X' unsafe in 'case' (line 27)"
+howToWriteMultipleCase() ->
+	%% will not compile, have run it in the REPL ie. erl
+	%% X & Y is bound here
+	P1 = case [2,3,4] of
+				 [X,Y|_] -> X+Y;
+				 [S] -> S;
+				 _ -> 0
+			 end,
+
+	%% X & Y is bound before, so will not match
+	%% additionally S is bound
+	P2 = case [10] of
+				 [X,Y|_] -> X+Y;
+				 [S] -> S;
+				 _ -> 0
+			 end,
+
+	%% X & Y is bound before, so will not match
+	%% also S is bound before, so will not match
+	%% even though in isolation it seems as if it will match S
+	P3 = case [6] of
+				 [X,Y|_] -> X+Y;
+				 [S] -> S;
+				 _ -> 0
+			 end,
+
+	P4 = case [] of
+				 [X,Y|_] -> X+Y;
+				 [S] -> S;
+				 _ -> 0
+			 end,
+
+	[P1, P2, P3, P4].
